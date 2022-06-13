402222439e9ab91b9a92fdf1a49e555e526b05c8
Dennis Rodrigo <62552509+Darispeg@users.noreply.github.com>
2022-06-12 22:37:50 -0400

    build

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(path.join(__dirname, 'src')));
+app.use(express.static('./dist/fuse'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'src/'}),
+    res.sendFile('index.html', {root: 'dist/fuse/'}),
 );
 
 app.get("*", (req, res) => {
