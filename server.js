a85a48937cef5afb7579703c231d14ab20cccbe2
Dennis Rodrigo <62552509+Darispeg@users.noreply.github.com>
2022-06-12 21:53:29 -0400

    06-02-22

--- server.js
+++ server.js
@@ -8,11 +8,11 @@ const app = express();
 app.use(express.static('./src/app/'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('dashboard.component.html', {root: 'src/app/modules/dashboard/'}),
+    res.sendFile('sign-in.component.html', {root: 'src/app/modules/auth/sign-in'}),
 );
 
 app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "build", "dashboard.component.html"));
+    res.sendFile(path.resolve(__dirname, "build", "sign-in.component.html"));
   });
 
 // Start the app by listening on the default Heroku port
