2cf819f5760de5bedd9e440dabdcf38df8c95771
Dennis Rodrigo <62552509+Darispeg@users.noreply.github.com>
2022-06-12 21:08:49 -0400

    change route

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/angular-app-heroku'));
+app.use(express.static('./dist/st-webapp'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/angular-app-heroku/'}),
+    res.sendFile('index.html', {root: 'dist/st-webapp/'}),
 );
 
 app.get("*", (req, res) => {
