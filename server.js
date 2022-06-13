a1d9222dab966e9da3a111b3857b928d32f6af7c
Dennis Rodrigo <62552509+Darispeg@users.noreply.github.com>
2022-06-12 21:47:15 -0400

    last

--- server.js
+++ server.js
@@ -5,14 +5,14 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./src/app/modules/dashboard/dashboard.component.html'));
+app.use(express.static('./src/app/'));
 
 app.get('/*', (req, res) =>
     res.sendFile('dashboard.component.html', {root: 'src/app/modules/dashboard/'}),
 );
 
 app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "build", "dashboard.component.html"));
   });
 
 // Start the app by listening on the default Heroku port
