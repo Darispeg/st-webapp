7684be326d4f71ec68d873ac93815d80971aeb2e
Dennis Rodrigo <62552509+Darispeg@users.noreply.github.com>
2022-06-12 22:26:24 -0400

    reverse

--- server.js
+++ server.js
@@ -1,32 +1,19 @@
+//Install express server
 const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
 const path = require('path');
+
 const app = express();
-const port = process.env.PORT || 3000
 
-app.use(express.static(path.join(__dirname, '/src')));
+// Serve only the static files form the dist directory
+app.use(express.static(path.join(__dirname, 'src')));
 
-app.use(bodyParser.json());
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'src/'}),
+);
 
-app.use(cors());
+app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "build", "index.html"));
+  });
 
-const forceSSL = function () {
-    return function (req, res, next) {
-      if (req.headers['x-forwarded-proto'] !== 'https') {
-        return res.redirect(
-          ['https://', req.get('Host'), req.url].join('')
-        );
-      }
-      next();
-    }}
-
-app.use(forceSSL());    
-
-app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname + '/src/index.html'));
-        });
-    
-    app.listen(port, () => {
-    console.log('Server started on port '+port);
-    });
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
