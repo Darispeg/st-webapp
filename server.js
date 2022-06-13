1151b171b7b5f408ec60e10b2a526175425643b8
Dennis Rodrigo <62552509+Darispeg@users.noreply.github.com>
2022-06-12 22:12:19 -0400

    src

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./src/'));
+app.use(express.static(path.join(__dirname, 'src')));
 
 app.get('/*', (req, res) =>
     res.sendFile('sign-in.component.html', {root: 'src/app/modules/auth/sign-in/'}),
